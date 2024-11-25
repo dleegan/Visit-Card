f6b33b0afc41e03f086dc3c21c18d3aef0be460c
DUPROS Leegan <leegan.dupros@epitech.eu>
2024-11-25 17:33:53 +0100

    Initial Commit

---
+++ Visit Card/Visit_CardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Visit_CardApp.swift
+//  Visit Card
+//
+//  Created by dleegan on 25/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct Visit_CardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
